1ace390b06a9891aae27b785ee624f536cc07947
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-01-31 18:08:10 -0600

    yesterdays work

---
+++ news-homepage-main/app.js
@@ -0,0 +1,12 @@
+var menu = document.getElementById("navigation-menu");
+var closeButton = document.getElementById("close-b");
+var burger = document.getElementById("burger-b");
+var menuItem = document.getElementsByTagName("li");
+
+function openMenu() {
+	menu.style.display = "block";
+}
+
+function closeMenu() {
+	menu.style.display = "none";
+}
